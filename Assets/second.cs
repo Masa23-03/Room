75c9bae83e352d496c6df00bf355e5a5a104aeeb
Masa Sabobeh <masasabobeh03@gmail.com>
2025-01-09 14:08:03 +0200

    I just updated my code, A very important update.

--- Assets/second.cs
+++ Assets/second.cs
@@ -7,7 +7,7 @@ public class second : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("This is my first use of github! \n");
     }
 
     // Update is called once per frame
