5ba57a158395a2c389a3370ec737e908b79445cf
Masa Sabobeh <masasabobeh03@gmail.com>
2025-01-09 17:53:38 +0200

    operator's overloading

--- Assets/second.cs
+++ Assets/second.cs
@@ -7,7 +7,12 @@ public class second : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("This is my first use of github! \n");
+        int a=4;
+        float b=2;
+        var c=b/a;
+
+        Debug.Log(c);
+
     }
 
     // Update is called once per frame
